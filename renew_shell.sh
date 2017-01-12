5e3960f930c149cef560f221bd3f92db9035a6f7
Thiago Roberto Lima Lopes <lolthiago@gmail.com>
2017-01-12 14:14:02 -0200

    Testing

--- renew_shell.sh
+++ renew_shell.sh
@@ -3,3 +3,4 @@
 #CRON
 #00 05 * * * 
 certbot renew --agree-tos --quiet --no-self-upgrade --renew-hook 'cp "$(sudo realpath $RENEWED_LINEAGE/cert.pem)" "$(sudo realpath $RENEWED_LINEAGE/cert.pem)-bkp-$(date +%y-%m-%d_%H:%M:%S)" && sudo cat "$RENEWED_LINEAGE/privkey.pem" >> "$RENEWED_LINEAGE/cert.pem"' --post-hook "systemctl reload httpd"
+#test
