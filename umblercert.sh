d4d60ddd1aee406d42327284f1ca78aeb07dc836
trlthiago <lolthiago@gmail.com>
2017-01-24 15:28:34 -0200

    jsd

--- umblercert.sh
+++ umblercert.sh
@@ -124,7 +124,7 @@ ConfigureRenewCron(){
     RemoveCron
     #sudo echo "00 05 * * * certbot renew --agree-tos --quiet --no-self-upgrade --renew-hook 'cp \"\$(sudo realpath \$RENEWED_LINEAGE/cert.pem)\" \"\$(sudo realpath \$RENEWED_LINEAGE/cert.pem)-bkp-\$(date +%y-%m-%d_%H:%M:%S)\" && sudo cat \"\$RENEWED_LINEAGE/privkey.pem\" >> \"\$RENEWED_LINEAGE/cert.pem\"' --post-hook \"systemctl reload httpd\"" >> /var/spool/cron/root
     
-    sudo echo "00 05 * * 6,0 (certbot-auto renew --agree-tos --quiet; systemctl reload httpd)" >> /var/spool/cron/root 
+    sudo echo "00 05 * * 6,0 (sudo certbot-auto renew --agree-tos --quiet; systemctl reload httpd)" >> /var/spool/cron/root 
     
 
     if [ -f /etc/debian_version ]; then
